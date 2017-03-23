5c1138e7722e1fcdcd28e737b30a58e5a5469dff
Mathieu Bolard <mathieu.bolard@gmail.com>
2017-03-23 11:49:20 +0100

    Initial Commit

---
+++ OuichBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OuichBar
+//
+//  Created by mathieu on 23/03/2017.
+//  Copyright © 2017 mathieu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OuichBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OuichBar
+//
+//  Created by mathieu on 23/03/2017.
+//  Copyright © 2017 mathieu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
